2058d678414aff144d04b968418ad81c2ba5edd6
Alemzhan-A <alimzanalimzag@gmail.com>
2024-07-25 13:28:44 +0500

    LESGO

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,23 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { query } = req.body;
     try {
-      const response = await fetch('https://web-production-8d99.up.railway.app/api/search', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query }),
-      });
-      if (!response.ok) {
-        throw new Error('Ошибка при поиске продуктов');
-      }
-      const data = await response.json();
-      console.log("ВОТ ЧТО ПРИНИМАЕМ")
-      console.log(data)
-      res.status(200).json(data);
+      const response = await axios.post('https://web-production-8d99.up.railway.app/api/search', 
+        { query }, 
+        { 
+          timeout: 80000, // 30 секунд
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+      res.status(200).json(response.data);
     } catch (error) {
       console.error('Ошибка при поиске:', error);
       res.status(500).json({ error: 'Произошла ошибка при обработке запроса' });
